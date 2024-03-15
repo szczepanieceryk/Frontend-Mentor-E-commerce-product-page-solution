7b17049073209f38bc746b9e423829b140b50bf5
eryk <e.szczepaniec@lightpixel.io>
2024-03-15 16:06:15 +0100

    Display 'Project in progress' message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./styles/style.css";
 
 const App = () => {
-  return <div className="app-wrapper"></div>;
+  return (
+    <div className="app-wrapper">
+      <div className="main-wrapper">
+        <h1>Project in progress...</h1>
+      </div>
+    </div>
+  );
 };
 
 export default App;
